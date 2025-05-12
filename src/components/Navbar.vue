fadcc35fcb3d7b346a763c101a1f0f77bad22ac4
d-hooper <dallinhoop3@gmail.com>
2025-05-11 21:11:45 -0600

    init

---
+++ src/components/Navbar.vue
@@ -0,0 +1,61 @@
+<script setup>
+import { ref, watch } from 'vue';
+import { loadState, saveState } from '../utils/Store.js';
+import Login from './Login.vue';
+
+const theme = ref(loadState('theme') || 'light')
+
+function toggleTheme() {
+  theme.value = theme.value == 'light' ? 'dark' : 'light'
+}
+
+watch(theme, () => {
+  document.documentElement.setAttribute('data-bs-theme', theme.value)
+  saveState('theme', theme.value)
+}, { immediate: true })
+
+</script>
+
+<template>
+  <nav class="navbar navbar-expand-md bg-light border-bottom border-indigo">
+    <div class="container gap-2">
+      <img class="navbar-brand" alt="logo" src="@/assets/img/Logo.png" height="50" />
+
+      <a href=""><span class="mdi"></span></a>
+      <a href=""><span class="mdi"></span></a>
+
+      <!-- collapse button -->
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbar-links"
+              aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="mdi mdi-menu text-light"></span>
+      </button>
+      <!-- collapsing menu -->
+      <!-- <div class="collapse navbar-collapse " id="navbar-links">
+        <ul class="navbar-nav">
+          <li>
+            <RouterLink :to="{ name: 'About' }" class="btn text-green selectable">
+              About
+            </RouterLink>
+          </li>
+        </ul>
+
+      </div> -->
+    </div>
+  </nav>
+</template>
+
+<style lang="scss" scoped>
+a {
+  text-decoration: none;
+}
+
+.nav-link {
+  text-transform: uppercase;
+}
+
+.navbar-nav .router-link-exact-active {
+  border-bottom: 2px solid var(--bs-success);
+  border-bottom-left-radius: 0;
+  border-bottom-right-radius: 0;
+}
+</style>
---
+++ src/pages/HomePage.vue
@@ -0,0 +1,16 @@
+<script setup>
+
+
+</script>
+
+<template>
+  <div class="container">
+    <div class="row">
+      <div class="col-12">
+
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped lang="scss"></style>
