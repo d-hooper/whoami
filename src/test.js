d548d69a2878cbab557cf9dc26dba02fd3289162
d-hooper <111197685+d-hooper@users.noreply.github.com>
2025-07-09 18:18:30 -0600

    updated file path for scss import; deploy

--- src/test.js
+++ src/test.js
@@ -1,5 +1,6 @@
 // import '@mdi/font/css/materialdesignicons.css'
-import '@/assets/scss/style.scss'
+// import '@/assets/scss/style.scss'
+import './src/assets/scss/style.scss'
 import 'bootstrap/dist/js/bootstrap.bundle.js'
 // import { createApp } from 'vue'
 // // @ts-ignore
